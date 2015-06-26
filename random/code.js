bbee71300867fd31f95b273d6114522f296ade44
Sami <Sami0@users.noreply.github.com>
2015-06-25 20:46:41 -0600

    Update code.js

--- random/code.js
+++ random/code.js
@@ -1,6 +1,6 @@
 function myNumber(){
  document.getElementById("RN").innerHTML =
-   " the number is "+ Math.floor(Math.random()*1000) + 1;
+   " the number is "+ Math.floor(Math.random()*1000);
 
 }
 
